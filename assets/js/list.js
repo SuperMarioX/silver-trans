877d2291780a219c373810bea86303091a96f0d0
lg <1057935399@qq.com>
2018-01-21 23:46:57 +0800

    mod

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,65 +1,75 @@
 //$(document).ready();
 
-$(function(){
-    ajaxSetup();
-    list('/');
+$(function() {
+	ajaxSetup();
+	list('/');
 });
 
 function ajaxSetup() {
-    $.ajaxSetup({
-        timeout : 30000,
-        beforeSend : function(xhr) {
-            xhr.setRequestHeader("Ajax_request", "true");
-        },
-        complete : function(xhr, ts) {
-            if (xhr.getResponseHeader('sessionState') == 'timeout' && xhr.status == 403) {
-                //window.location.href = "login.jsp";//xhr.statusText
-                console.log('jump');
-            }
-        }
-    });
+	$.ajaxSetup({
+		timeout : 30000,
+		beforeSend : function(xhr) {
+			xhr.setRequestHeader("Ajax_request", "true");
+		},
+		complete : function(xhr, ts) {
+			if (xhr.getResponseHeader('sessionState') == 'timeout'
+					&& xhr.status == 403) {
+				// window.location.href = "login.jsp";//xhr.statusText
+				console.log('jump');
+			}
+		}
+	});
 }
 
 function jump(a) {
-    var str = '';
-    if(a != undefined){
-        var path = $("#path").text().trim();
-        if($(a).text().trim().endsWith('..')) {
-            str = path + '..';
-        }
-        else{
-            str = path=='/' ? '' : path;
-            str = str+'/'+$(a).text();
-        }
-    }
-    list(str);
+	var str = '';
+	if (a != undefined) {
+		var path = $("#path").text().trim();
+		if ($(a).text().trim().endsWith('..')) {
+			str = path + '..';
+		} else {
+			str = path == '/' ? '' : path;
+			str = str + '/' + $(a).text();
+		}
+	}
+	list(str);
 }
 
-function list(str){
-    $.ajax({
-        url : "/list.action",
-        type : "GET",
-        data : {path:str},
-        dataType:"json",
-        async : true,
-        success : function(data) {
-        $("ul").html("");
-        if (data.path==''){
-            $("#path").html('/&nbsp;&nbsp;');
-        }else{
-            $("#path").text(data.path);
-            $("ul").append("<li><a href='javascript:void(0);' onclick=jump(this)>..&nbsp;&nbsp;&nbsp;&nbsp;</a></li>");
-        }
-        for (i in data.dirs) {
-            var s="<li>"+$("#dir_ico").html()+"<a href='javascript:void(0);' onclick=jump(this)>" + data.dirs[i]+"</a></li>";
-            $("ul").append(s);
-        }
-        for (i in data.files) {
-            var str = $("#path").text()=='/' ? '' : $("#path").text();
-            str = 'list.action?path='+str+'/'+data.files[i];
-            var s="<li>"+$("#file_ico").html()+"<a href='"+str+"'>"+ data.files[i]+"</a></li>";
-            $("ul").append(s);
-        }
-        }
-    });
+function list(str) {
+	$
+			.ajax({
+				url : "/list.action",
+				type : "GET",
+				data : {
+					path : str
+				},
+				dataType : "json",
+				async : true,
+				success : function(data) {
+					$("ul").html("");
+					if (data.path == '') {
+						$("#path").html('/&nbsp;&nbsp;');
+					} else {
+						$("#path").text(data.path);
+						$("ul")
+								.append(
+										"<li><a href='javascript:void(0);' onclick=jump(this)>..&nbsp;&nbsp;&nbsp;&nbsp;</a></li>");
+					}
+					for (i in data.dirs) {
+						var s = "<li>"
+								+ $("#dir_ico").html()
+								+ "<a href='javascript:void(0);' onclick=jump(this)>"
+								+ data.dirs[i] + "</a></li>";
+						$("ul").append(s);
+					}
+					for (i in data.files) {
+						var str = $("#path").text() == '/' ? '' : $("#path")
+								.text();
+						str = 'list.action?path=' + str + '/' + data.files[i];
+						var s = "<li>" + $("#file_ico").html() + "<a href='"
+								+ str + "'>" + data.files[i] + "</a></li>";
+						$("ul").append(s);
+					}
+				}
+			});
 }
--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,54 +1,56 @@
 var r = new Resumable({
-  target: 'upload.action',
-  method: 'octet',
-  chunkSize:1024*300,
-  simultaneousUploads:4,
-  //testChunks: true,
-  query:{upload_token: new Date().getTime()},
-  throttleProgressCallbacks:1
+	target : 'upload.action',
+	method : 'octet',
+	chunkSize : 1024 * 300,
+	simultaneousUploads : 4,
+	// testChunks: true,
+	query : {
+		upload_token : new Date().getTime()
+	},
+	throttleProgressCallbacks : 1
 });
 
 r.assignBrowse(document.getElementById('browseButton'));
 r.assignDrop(document.getElementById('dropTarget'));
 
-r.on('fileSuccess', function(file){
-    console.info('fileSuccess',file);
-  });
-r.on('fileProgress', function(file){
-    //console.info('fileProgress', file);
-  });
-r.on('fileAdded', function(file, event){
-    $("#fileName").html(file.fileName)
-    r.upload();
-    file.relativePath = $("#path").text().trim();
-    console.info('fileAdded', file);
-  });
-r.on('filesAdded', function(array){
-    r.upload();
-    console.info('filesAdded', array);
-  });
-r.on('fileRetry', function(file){
-    console.info('fileRetry', file);
-  });
-r.on('fileError', function(file, message){
-    console.info('fileError', file, message);
-  });
-r.on('uploadStart', function(){
-    console.info('uploadStart');
-  });
-r.on('complete', function(){
-    console.info('complete');
-  });
-r.on('progress', function(){
-    var process =  Math.round(r.progress()*10000)/100;
-    $('#process').text(process+'%')
-  });
-r.on('error', function(message, file){
-    console.info('error', message, file);
-  });
-r.on('pause', function(){
-    console.info('pause');
-  });
-r.on('cancel', function(){
-    console.info('cancel');
-  });
+r.on('fileSuccess', function(file) {
+	console.info('fileSuccess', file);
+});
+r.on('fileProgress', function(file) {
+	// console.info('fileProgress', file);
+});
+r.on('fileAdded', function(file, event) {
+	$("#fileName").html(file.fileName)
+	r.upload();
+	file.relativePath = $("#path").text().trim();
+	console.info('fileAdded', file);
+});
+r.on('filesAdded', function(array) {
+	r.upload();
+	console.info('filesAdded', array);
+});
+r.on('fileRetry', function(file) {
+	console.info('fileRetry', file);
+});
+r.on('fileError', function(file, message) {
+	console.info('fileError', file, message);
+});
+r.on('uploadStart', function() {
+	console.info('uploadStart');
+});
+r.on('complete', function() {
+	console.info('complete');
+});
+r.on('progress', function() {
+	var process = Math.round(r.progress() * 10000) / 100;
+	$('#process').text(process + '%')
+});
+r.on('error', function(message, file) {
+	console.info('error', message, file);
+});
+r.on('pause', function() {
+	console.info('pause');
+});
+r.on('cancel', function() {
+	console.info('cancel');
+});
